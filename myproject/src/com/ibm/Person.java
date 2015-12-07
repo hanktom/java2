f84b37e3f69d79f77dcc29f75374b11c517317a4
Hank Tom <hanktom@gmail.com>
2015-12-07 18:50:52 +0800

    init commit

---
+++ myproject/src/com/ibm/Person.java
@@ -0,0 +1,13 @@
+package com.ibm;
+
+public class Person {
+	
+	public void sayHello(){
+		System.out.println("HELLO");
+	}
+	
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.sayHello();
+	}
+}
