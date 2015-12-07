948134092e0460385eae88b985e6b4ea9cff54a1
Hank Tom <hanktom@gmail.com>
2015-12-07 20:58:24 +0800

    init commit

--- myproject/src/com/ibm/Hello.java
+++ myproject/src/com/ibm/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO");
+		int i =5;
+		double d =i/4.0;
+		System.out.println(d);
 	}
 
 }
--- myproject/src/com/ibm/Person.java
+++ myproject/src/com/ibm/Person.java
@@ -1,13 +1,29 @@
 package com.ibm;
 
 public class Person {
+	float weight;
+	float height;
+	public Person(float weight, float height){
+		this.weight = weight;
+		this.height = height;
+	}
+	
+	public float bmi(){
+		float bmi = weight/(height*height);
+		bmi = ((int)(bmi*100))/100.0f ;
+		return bmi;
+	}
 	
 	public void sayHello(){
 		System.out.println("HELLO");
 	}
 	
 	public static void main(String[] args) {
-		Person p = new Person();
+		Person p = new Person(65.5f, 1.7f);
 		p.sayHello();
+//		p.weight = 65.5f;
+//		p.height = 1.7f;
+		System.out.println(p.bmi());
+		
 	}
 }
