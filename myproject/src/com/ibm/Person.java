b23fb59fdf4e0a12e885b23e580ff6b7909c7e28
Hank Tom <hanktom@gmail.com>
2015-12-10 19:30:04 +0800

    比較,if..else,Person.advice

--- myproject/src/com/ibm/Person.java
+++ myproject/src/com/ibm/Person.java
@@ -8,6 +8,21 @@ public class Person {
 		this.height = height;
 	}
 	
+	public String advice(){
+		float bmi = bmi();
+		String msg;
+		if (bmi<16){
+			msg = "多吃點哦";
+		}else{
+			if (bmi>25){
+				msg = "少吃一點哦";
+			}else{
+				msg = "正常";
+			}
+		}
+		return msg;
+	}
+	
 	public float bmi(){
 		float bmi = weight/(height*height);
 		bmi = ((int)(bmi*100))/100.0f ;
@@ -19,12 +34,17 @@ public class Person {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println(6!=3);
+		
 		Person p = new Person(65.5f, 1.7f);
 		p.sayHello();
 //		p.weight = 65.5f;
 //		p.height = 1.7f;
-		System.out.println(p.bmi());
-		Person p2;
+		
+		System.out.println(p.advice());
+		
 		
 	}
+	
+	
 }
