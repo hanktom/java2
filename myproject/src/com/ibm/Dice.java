14f8e23af54ccfe4a1d043218bdcbefd6e00cba4
Hank Tom <hanktom@gmail.com>
2015-12-14 20:36:51 +0800

    while迴圈,算骰子出現6,6,6,6

--- myproject/src/com/ibm/Dice.java
+++ myproject/src/com/ibm/Dice.java
@@ -7,12 +7,22 @@ public class Dice {
 	public static void main(String[] args) {
 		Random r = new Random();
 		int[] n = new int[4];
-		for (int t = 0; t < 10; t++) {
+		boolean allSix = false;
+		int time= 0;
+		while(!allSix ){
+			time++;
+			int sum = 0;
 			for (int i = 0; i < 4; i++) {
 				n[i] = r.nextInt(6) + 1;
 				System.out.print(n[i] + " ");
+				sum=sum+n[i];
 			}
 			System.out.println();
+			if (sum==24){
+				allSix = true;
+				System.out.println(time);
+			}
 		}
+		
 	}
 }
