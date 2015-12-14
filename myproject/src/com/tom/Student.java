72cbc4fda13ac44f354a45126e8de6ac9759b209
Hank Tom <hanktom@gmail.com>
2015-12-14 19:16:36 +0800

    類別層級的屬性(Student.pass)

---
+++ myproject/src/com/tom/Student.java
@@ -0,0 +1,36 @@
+package com.tom;
+
+public class Student {
+	int math;
+	int chinese;
+	int english;
+	static int pass = 60;
+	public Student(int math, int chinese, int english){
+		this.math = math;
+		this.chinese = chinese;
+		this.english = english;
+	}
+	
+	public int getAverage(){
+		int avg = (math+chinese+english)/3;
+		return avg;
+	}
+	
+	public void print(){
+		System.out.print(math+"\t"+chinese+"\t"+english+"\t"+getAverage());
+		if (getAverage()<pass)
+			System.out.println("*");
+		else
+			System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Student stu1 = new Student(60, 44,  70);
+		Student stu2 = new Student(35, 77, 66);
+		Student.pass = 55;
+		stu1.print();
+		stu2.print();
+		stu1.english = 55;
+//		System.out.println(stu.getAverage());
+	}
+}
