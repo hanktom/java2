b663ab0f99e6dc77b6c41be47db999137bb510a0
Hank Tom <hanktom@gmail.com>
2015-12-21 19:27:22 +0800

    樂透建構子x3, validate重覆

--- myproject/src/com/ibm/Lottery539.java
+++ myproject/src/com/ibm/Lottery539.java
@@ -5,6 +5,38 @@ import java.util.Random;
 public class Lottery539 {
 	private int[] nums = new int[5];
 	
+	public Lottery539(){
+		generate();
+	}
+	
+	public Lottery539(int n1, int n2, int n3, int n4, int n5){
+		nums[0] = n1;
+		nums[1] = n2;
+		nums[2] = n3;
+		nums[3] = n4;
+		nums[4] = n5;
+	}
+	
+	public Lottery539(int[] nums){
+		this.nums = nums;
+	}
+
+	protected boolean validate(){
+		int[] spots = new int[39];
+		for (int i=0;i<5;i++){
+			spots[nums[i]-1] = 1;
+		}
+		int sum=0;
+		for (int i=0;i<39;i++){
+			sum=sum+spots[i];
+		}
+		if (sum<5)
+			return false;
+		else
+			return true;
+		
+	}
+	
 	void generate(){
 		Random r = new Random();
 		int[] spots = new int[40];
@@ -47,9 +79,11 @@ public class Lottery539 {
 	}
 	
 	public static void main(String[] args) {
-		Lottery539 lot = new Lottery539();
+		/*Lottery539 lot = new Lottery539();
 		lot.generate();
-		lot.print();
+		lot.print();*/
+		Lottery539 lot1 = new Lottery539(3, 5, 9, 10,3);
+		System.out.println(lot1.validate());
 	}
 
 }
