c2bdb30467157b7c7d064f2c5eb5475335260268
Hank Tom <hanktom@gmail.com>
2015-12-24 20:19:18 +0800

    FileReader

--- myproject/src/com/file/SimpleFileOut.java
+++ myproject/src/com/file/SimpleFileOut.java
@@ -1,11 +1,28 @@
 package com.file;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class SimpleFileOut {
 
 	public static void main(String[] args) throws IOException {
+//		writeFile();
+		FileReader in = new FileReader("data.txt");
+		int d = in.read();
+		System.out.println((char)d);
+		d = in.read();
+		System.out.println(d);
+		d = in.read();
+		System.out.println(d);
+		d = in.read();
+		System.out.println(d);
+		d = in.read();
+		System.out.println(d);
+		
+	}
+
+	private static void writeFile() throws IOException {
 		FileWriter out = new FileWriter("data.txt");
 		out.write("ABC");
 		out.flush();
