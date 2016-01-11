594049f22abf3fc6508223268f75044c1a3d74e1
Hank Tom <hanktom@gmail.com>
2016-01-11 21:03:17 +0800

    Lambda

--- myproject/src/com/thread/Racing6.java
+++ myproject/src/com/thread/Racing6.java
@@ -3,37 +3,44 @@ package com.thread;
 public class Racing6 {
 
 	public static void main(String[] args) {
-		Thread h = new Thread(){
+		Thread h = new Thread() {
 
 			@Override
 			public void run() {
-				for (int i=1; i<=500; i++){
-					System.out.println(getName()+":"+i);
+				for (int i = 1; i <= 500; i++) {
+					System.out.println(getName() + ":" + i);
 				}
 			}
-			
+
 		};
 		h.start();
 		//
 		Runnable hr = new Runnable() {
-			
+
 			@Override
 			public void run() {
-				for (int i=1; i<=500; i++){
-					System.out.println("H2:"+i);
+				for (int i = 1; i <= 500; i++) {
+					System.out.println("H2:" + i);
 				}
-				
+
 			}
 		};
+		// Lambda
+		Runnable hr2 = () -> {
+			for (int i = 1; i <= 500; i++) {
+				System.out.println("H2:" + i);
+			}
+		};
+
 		Thread thr = new Thread(hr);
 		thr.start();
 		//
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				for (int i=1; i<=500; i=i+2){
-					System.out.println("H3:"+i);
+				for (int i = 1; i <= 500; i = i + 2) {
+					System.out.println("H3:" + i);
 				}
 			}
 		}).start();
